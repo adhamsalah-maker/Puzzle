0fa8a4105875e48241c043dd297c8fa1008773dc
cherrak adham salah <adham-salah.cherrak9@etu.univ-lorraine.fr>
2026-03-24 15:56:26 +0100

    modification de GrigUtils pour avoir le nombre exact de piece

--- app/src/main/java/fr/ul/puzzle/utils/GridUtils.java
+++ app/src/main/java/fr/ul/puzzle/utils/GridUtils.java
@@ -3,18 +3,32 @@ package fr.ul.puzzle.utils;
 public class GridUtils {
 
     public static int[] calculerGrille(int largeurImage, int hauteurImage, int nbPiecesSouhaite) {
-        double ratio = (double) largeurImage / hauteurImage;
 
-        int nbColonnes = (int) Math.round(Math.sqrt(nbPiecesSouhaite * ratio));
-        if (nbColonnes < 1) {
-            nbColonnes = 1;
+        boolean imagePaysage = largeurImage >= hauteurImage;
+
+        switch (nbPiecesSouhaite) {
+            case 16:
+                return new int[]{4, 4};
+
+            case 32:
+                if (imagePaysage) {
+                    return new int[]{4, 8};
+                } else {
+                    return new int[]{8, 4};
+                }
+
+            case 64:
+                return new int[]{8, 8};
+
+            case 128:
+                if (imagePaysage) {
+                    return new int[]{8, 16};
+                } else {
+                    return new int[]{16, 8};
+                }
+
+            default:
+                return new int[]{4, 4};
         }
-
-        int nbLignes = (int) Math.round((double) nbPiecesSouhaite / nbColonnes);
-        if (nbLignes < 1) {
-            nbLignes = 1;
-        }
-
-        return new int[]{nbLignes, nbColonnes};
     }
 }
